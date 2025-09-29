b3aa1c142c8db181c60354fa4d3ee3b2793b3b81
Shreekant14 <165187142+Shreekant14@users.noreply.github.com>
2025-09-29 13:04:50 +0530

    Add files via upload

---
+++ prg5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Car{
+public:
+string bramd;
+int age;
+void display(){
+cout<<"Name :"<<name<<"\nAge:"<<age<<endl;
+}
+};
+int main()
+{
+Student s1;
+s1.name = "Shreekant";
+s1.age = 19;
+s1.display();
+return 0;
+}
