c6c27adf56dc40ca811b4e03c6b403c82fa9f98b
Shreekant14 <165187142+Shreekant14@users.noreply.github.com>
2025-09-29 12:36:40 +0530

    Add files via upload

---
+++ prg2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+int num ;
+printf("Enter a integer :");
+scanf("%d",&num);
+if(num % 2 ==0)
+printf("the number is even\n");
+else
+printf("the number is odd\n");
+return 0;
+}
